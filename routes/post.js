2673abacfef09d087aabae966ffa17b50c2cd6b6
Felix Cadieux <cadieuxfelix@outlook.com>
2024-12-05 19:17:51 -0500

    post route for posts

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('/', (req,res) => {
-    res.send('Hello World');
+// Getting all posts
+router.get('/', async (req,res) => {
+    try{
+        const post = await Post.find();
+        res.json(post)
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
 })
 
 // Getting all posts of a single user
 router.get('/:userid', (req,res) => {
-    res.send(req.params.userid)
+    res.send(req.params.userid);
 })
 
 // Get a single post
@@ -16,8 +23,22 @@ router.get('/id', (req,res) => {
 })
 
 // Creating a post logged in as a user
-router.post('/', (req,res) => {
-    
+router.post('/', async (req,res) => {
+    const post = new Post({
+        ownerId: req.body.ownerId,
+        content: req.body.content,
+        comments: req.body.comments,
+        caption: req.body.caption,
+        date: req.body.date,
+        likedBy: req.body.likedBy
+    });
+
+    try{
+        const newPost = await post.save();
+        res.status(201).json(newPost);
+    }catch (err) {
+        res.status(400).json({message: err.message});
+    }
 })
 // Deleting a post in you postHistory
 router.delete('/:id', (req,res) => {
