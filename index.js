054f10a18db40db47c768d55879fe57f44c94791
Felix Cadieux <cadieuxfelix@outlook.com>
2025-01-12 18:08:53 -0500

    CORS debug 9

--- index.js
+++ index.js
@@ -16,26 +16,19 @@ const uri = process.env.URI;
 
 // app.use(cors(corsOptions));
 // app.options('*', cors(corsOptions));
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://another-social-media-app.onrender.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
+const corsOptions = {
+  origin: 'https://another-social-media-app.onrender.com',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Content-Type-Options', 'Accept', 'X-Requested-With', 'Origin'],
+  credentials: true,
+  maxAge: 7200
+};
 
-  next();
-});
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(uri)
 .then(() => {
