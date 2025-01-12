a289ce882cf66ab8535043cc366241dc4f9bb376
Felix Cadieux <cadieuxfelix@outlook.com>
2025-01-12 16:11:12 -0500

    debug CORS 2

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const app = express();
 
 
 const uri = process.env.URI;
-const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: 'https://another-social-media-app.onrender.com',
+//   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.use(cors(corsOptions));
+// app.options('*', cors(corsOptions));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://another-social-media-app.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
