c90a4c03279393c59ae9d61ff4f64d56de7479dd
Felix Cadieux <cadieuxfelix@outlook.com>
2025-01-12 16:32:31 -0500

    CORS debug 6

--- index.js
+++ index.js
@@ -16,15 +16,23 @@ const uri = process.env.URI;
 
 // app.use(cors(corsOptions));
 // app.options('*', cors(corsOptions));
-app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://another-social-media-app.onrender.com'); // Allow your frontend origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS'); // Allow the required methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow necessary headers
-
-  if (req.method === 'OPTIONS') {
-      return res.status(204).end(); // Respond OK for preflight
-  }
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
   next();
 });
